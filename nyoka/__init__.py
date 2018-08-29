db4d7c445c25d297727b6365bbb7c99bc708aaf8
Nirmal Borah <nirmalborah93@gmail.com>
2018-08-29 15:29:40 +0530

    Updated not to include statsmodels and keras exporter for now.

--- nyoka/__init__.py
+++ nyoka/__init__.py
@@ -1,6 +1,6 @@
 from nyoka.nyoka.skl.skl_to_pmml import skl_to_pmml
-from nyoka.nyoka.statsmodels.arima import ArimaToPMML
-from nyoka.nyoka.statsmodels.exponential_smoothing import ExponentialSmoothingToPMML
-from nyoka.nyoka.keras.keras_model_to_pmml import KerasToPmml
+# from nyoka.nyoka.statsmodels.arima import ArimaToPMML
+# from nyoka.nyoka.statsmodels.exponential_smoothing import ExponentialSmoothingToPMML
+# from nyoka.nyoka.keras.keras_model_to_pmml import KerasToPmml
 from nyoka.nyoka.xgboost.xgboost_to_pmml import xgboost_to_pmml
 from nyoka.nyoka.lgbm.lgb_to_pmml import lgb_to_pmml
