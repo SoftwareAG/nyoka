1e803469ee78f58905f3f57b955bb13ca20f4060
Nirmal Borah <nirmalborah93@gmail.com>
2018-09-27 14:41:31 +0530

    Updated with new version

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup, find_packages
-from os import path
 
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+	long_description = f.read()
 
 setup(
 	name = "nyoka",
