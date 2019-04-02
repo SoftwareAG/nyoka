45a2730166d2d9379c999b20c577087dd526723c
Nirmal-neel <nirmalborah93@gmail.com>
2019-04-02 16:00:32 +0530

    Minor change

--- nyoka/metadata.py
+++ nyoka/metadata.py
@@ -1,3 +1,3 @@
-__version__ = '3.1.0'
+__version__ = '3.0.7'
 __license__ = "Apache Software License"
 
--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ setup(
 	],
 	packages = find_packages(),
 	install_requires = [
-		"lxml",
-		"numpy"
+		"lxml"
 	]
 )
