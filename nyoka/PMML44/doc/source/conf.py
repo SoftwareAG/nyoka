af4c049c705909b5f7f5ce50be33ca79fe9000bb
nyoka-pmml <42647531+nyoka-pmml@users.noreply.github.com>
2019-05-15 10:58:06 +0530

    Update conf.py

--- nyoka/PMML44/doc/source/conf.py
+++ nyoka/PMML44/doc/source/conf.py
@@ -19,8 +19,9 @@
 #
 import os
 import sys
-sys.path.append(os.path.join(os.curdir, "scripts"))
+# sys.path.append(os.path.join(os.curdir, "scripts"))
 # sys.path.insert(0, os.path.abspath('.'))
+sys.path.append('../../../..')
 
 
 # -- General configuration ------------------------------------------------
