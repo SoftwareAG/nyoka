8c80f266295d553ca7198d7535d1692458304c01
Chiragasourabh <chiragasourabh@gmail.com>
2019-07-03 16:10:46 +0530

    Test Cases

--- nyoka/tests/test_import.py
+++ nyoka/tests/test_import.py
@@ -6,12 +6,13 @@ class TestImport(unittest.TestCase):
         import nyoka
         self.assertEqual(hasattr(nyoka,"__version__"),True)
         self.assertEqual(hasattr(nyoka,"__license__"),True)
-        self.assertEqual(hasattr(nyoka,"skl_to_pmml"),True)
+        # self.assertEqual(hasattr(nyoka,"skl_to_pmml"),True)
+        self.assertEqual(hasattr(nyoka,"model_to_pmml"),True)
         self.assertEqual(hasattr(nyoka,"statsmodels_to_pmml"),True)
-        self.assertEqual(hasattr(nyoka,"KerasToPmml"),True)
-        self.assertEqual(hasattr(nyoka,"xgboost_to_pmml"),True)
-        self.assertEqual(hasattr(nyoka,"lgb_to_pmml"),True)
-        self.assertEqual(hasattr(nyoka,"MaskrcnnToPMML"),True)
+        # self.assertEqual(hasattr(nyoka,"KerasToPmml"),True)
+        # self.assertEqual(hasattr(nyoka,"xgboost_to_pmml"),True)
+        # self.assertEqual(hasattr(nyoka,"lgb_to_pmml"),True)
+        # self.assertEqual(hasattr(nyoka,"MaskrcnnToPMML"),True)
 
 if __name__=='__main__':
     unittest.main(warnings='ignore')
