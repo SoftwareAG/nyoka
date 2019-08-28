7ef9d44343fe4161f7920cda4e89f31f05cf9fac
Nirmal-neel <nirmalborah93@gmail.com>
2019-08-28 13:07:26 +0530

    added few more tests for retinanet

--- nyoka/tests/test_retinanet_to_pmml_UnitTest.py
+++ nyoka/tests/test_retinanet_to_pmml_UnitTest.py
@@ -9,29 +9,72 @@ from nyoka import PMML44 as pml
 
 class TestMethods(unittest.TestCase):
 
-    
-    def test_01(self):
-        
+    @classmethod
+    def setUpClass(self):
         url = 'https://github.com/fizyr/keras-retinanet/releases/download/0.5.1/resnet50_coco_best_v2.1.0.h5'
         r = requests.get(url)
 
         with open('resnet50_coco_best_v2.1.0.h5', 'wb') as f:
             f.write(r.content)
+    
+    def test_01(self):
+        
         model = load_model('resnet50_coco_best_v2.1.0.h5', backbone_name='resnet50')
         backbone = 'resnet'
         RetinanetToPmml(
             model,
             input_shape=(224,224,3),
             backbone_name=backbone,
-            pmml_file_name="retinanet_with_coco.pmml"
+            pmml_file_name="retinanet_with_coco_1.pmml"
         )
-        recon_pmml_obj = pml.parse("retinanet_with_coco.pmml",True)
+        recon_pmml_obj = pml.parse("retinanet_with_coco_1.pmml",True)
         binary_buffered = recon_pmml_obj.DataDictionary.DataField[0].Extension[0].value
         self.assertEqual(binary_buffered,'true')
         function = recon_pmml_obj.DeepNetwork[0].LocalTransformations.DerivedField[0].Apply.function
         self.assertEqual(function,'KerasRetinaNet:getBase64StringFromBufferedInput')
         scaling = recon_pmml_obj.DeepNetwork[0].LocalTransformations.DerivedField[0].Apply.Constant[0].valueOf_
         self.assertEqual(scaling,'caffe')
+
+    def test_02(self):
+        
+        model = load_model('resnet50_coco_best_v2.1.0.h5', backbone_name='resnet50')
+        backbone = 'mobilenet'
+        RetinanetToPmml(
+            model,
+            input_shape=(224,224,3),
+            backbone_name=backbone,
+            pmml_file_name="retinanet_with_coco_2.pmml"
+        )
+        recon_pmml_obj = pml.parse("retinanet_with_coco_2.pmml",True)
+        binary_buffered = recon_pmml_obj.DataDictionary.DataField[0].Extension[0].value
+        self.assertEqual(binary_buffered,'true')
+        function = recon_pmml_obj.DeepNetwork[0].LocalTransformations.DerivedField[0].Apply.function
+        self.assertEqual(function,'KerasRetinaNet:getBase64StringFromBufferedInput')
+        scaling = recon_pmml_obj.DeepNetwork[0].LocalTransformations.DerivedField[0].Apply.Constant[0].valueOf_
+        self.assertEqual(scaling,'tf')
+
+    def test_03(self):
+        
+        model = load_model('resnet50_coco_best_v2.1.0.h5', backbone_name='resnet50')
+        with self.assertRaises(AssertionError):
+            RetinanetToPmml(
+            model,
+            input_shape=(224,224,3),
+            backbone_name='resnet50',
+            pmml_file_name="retinanet_with_coco_2.pmml"
+        )
+
+    def test_04(self):
+        
+        model = load_model('resnet50_coco_best_v2.1.0.h5', backbone_name='resnet50')
+        with self.assertRaises(AssertionError):
+            RetinanetToPmml(
+            model,
+            input_shape=(224,224,3),
+            backbone_name='resnet50',
+            input_data='my_data',
+            pmml_file_name="retinanet_with_coco_2.pmml"
+        )
 
 
 if __name__=='__main__':
