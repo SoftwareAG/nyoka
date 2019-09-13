43e85b66a962ae6a9f6ef70a1993f06dd38d55c2
Nirmal-neel <nirmalborah93@gmail.com>
2019-09-13 17:40:32 +0530

    Updated for test

--- nyoka/tests/adapaUtilities.py
+++ nyoka/tests/adapaUtilities.py
@@ -11,9 +11,9 @@ import numpy
 class AdapaUtility:
 
     def __init__(self):
-        self.endpoint = os.environ['DOCKER_ADAPA_URL']
-        self.username = os.environ['DOCKER_ADAPA_UN']
-        self.password = os.environ['DOCKER_ADAPA_PW']
+        self.endpoint = os.environ['ADAPA_URL']
+        self.username = os.environ['ADAPA_UN']
+        self.password = os.environ['ADAPA_PW']
 
     def upload_to_zserver(self, file_name):
         files = {'file': open(file_name,'r')}
