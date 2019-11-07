2433583b97a04249fe7539a754316f1ec9516f90
nirmal-neel <nirmalborah93@gmail.com>
2019-11-07 12:09:23 +0530

    updated tests

--- nyoka/tests/adapaUtilities.py
+++ nyoka/tests/adapaUtilities.py
@@ -32,9 +32,7 @@ class AdapaUtility:
         return res.status_code
 
     def score_single_record(self, model_name):
-        self.delete_all_models()
         res = requests.get(self.endpoint+"apply/"+model_name, auth = HTTPBasicAuth(self.username, self.password))
-        print(res.json())
         return res.json()['outputs'][0]
 
     def score_in_zserver(self, model_name, test_file, model_type=None):
