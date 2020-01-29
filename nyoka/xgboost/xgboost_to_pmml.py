2da5d2af5dbc60509ffbc656714515b26dec7d9a
Nirmal-Neel <nirmalborah93@gmail.com>
2020-01-29 15:30:07 +0530

    Minor update

--- nyoka/xgboost/xgboost_to_pmml.py
+++ nyoka/xgboost/xgboost_to_pmml.py
@@ -65,7 +65,6 @@ def xgboost_to_pmml(pipeline, col_names, target_name, pmml_f_name='from_xgboost.
         pmml = pml.PMML(
             version=PMML_SCHEMA.VERSION.value,
             Header=sklToPmml.get_header(description),
-            MiningBuildTask=sklToPmml.get_mining_buildtask(pipeline),
             DataDictionary=sklToPmml.get_data_dictionary(model, col_names, target_name, categoric_values),
             **trfm_dict_kwargs,
             **PMML_kwargs
