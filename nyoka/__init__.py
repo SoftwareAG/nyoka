2233ad8b29ba38608928f3fddae997972857df42
Nirmal-Neel <nirmalborah93@gmail.com>
2020-08-25 14:46:24 +0530

    Removed deprecated class ArimaToPmml

--- nyoka/__init__.py
+++ nyoka/__init__.py
@@ -18,11 +18,9 @@
  """
 from nyoka.skl.skl_to_pmml import skl_to_pmml
 from nyoka.statsmodels.statsmodels_to_pmml import StatsmodelsToPmml
-from nyoka.statsmodels.arima import ArimaToPMML
 from nyoka.statsmodels.exponential_smoothing import ExponentialSmoothingToPMML
 from nyoka.keras.keras_model_to_pmml import KerasToPmml
 from nyoka.xgboost.xgboost_to_pmml import xgboost_to_pmml
 from nyoka.lgbm.lgb_to_pmml import lgb_to_pmml
 from nyoka.object_detection.retinanet import RetinanetToPmml
 from metadata import __version__, __license__
-
