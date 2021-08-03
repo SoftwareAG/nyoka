cd29b9d145919a0f2552bec6c4cbabac2b8e1c92
Nirmal <nirmal.borah@softwareag.com>
2021-08-03 13:13:45 +0530

    Version updated from 5.0.0 to 5.0.1

--- nyoka/metadata.py
+++ nyoka/metadata.py
@@ -16,6 +16,6 @@
    See the License for the specific language governing permissions and
    limitations under the License.
  """
-__version__ = '5.0.0'
+__version__ = '5.0.1'
 __license__ = "Apache Software License"
 
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 setup(
 	name = "nyoka",
 	version = __version__,
-	description = "Python library for converting a large number of ML / DL models to PMML",
+	description = "Python library for converting a large number of ML models to PMML",
 	long_description = long_description,
 	long_description_content_type="text/markdown",
 	author = "maintainer",
