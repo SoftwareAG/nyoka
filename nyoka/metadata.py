19beb699505b6620d1849c40acda6de8af8398a7
Vinay Kumar Ranganath Babu <vinayvinkumar@gmail.com>
2022-05-05 09:36:28 +0530

    Update metadata.py

--- nyoka/metadata.py
+++ nyoka/metadata.py
@@ -16,6 +16,6 @@
    See the License for the specific language governing permissions and
    limitations under the License.
  """
-__version__ = '5.2.1'
+__version__ = '5.3.0'
 __license__ = "Apache Software License"
 
